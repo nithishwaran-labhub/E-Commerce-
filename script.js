51d230a0652644f4294ac8c22331164a379c127d
nithishwaran-labhub <nithishwaran321io@gmail.com>
2025-11-19 23:44:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let products = [];
+
+fetch("data/products.json")
+  .then(response => response.json())
+  .then(data => {
+      products = data;
+      displayProducts();      // Display on home page
+      displayCart();          // Display cart items if any
+      displayWishlist();      // Display wishlist items if any
+  })
+  .catch(error => console.error("Error loading products:", error));
 
